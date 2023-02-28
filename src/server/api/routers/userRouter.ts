efcf2d73844a581d32c06a452171d410a8d9e4f8
Louis Qian <yuq4@illinois.edu>
2023-02-28 14:10:22 -0600

    feat: getUser by email

--- src/server/api/routers/userRouter.ts
+++ src/server/api/routers/userRouter.ts
@@ -8,10 +8,20 @@ const prisma = new PrismaClient();
 export const userRouter = createTRPCRouter({
   // create a new user
   userCreate: publicProcedure
-    .input(z.object({ email: z.string().email() }))
+    .input(z.object({ email: z.string().email(), name: z.string() }))
     .mutation((req) => {
       return prisma.user.create({
         data: {
+          email: req.input.email,
+          name: req.input.name,
+        },
+      });
+    }),
+  getUser: publicProcedure
+    .input(z.object({ email: z.string().email() }))
+    .query((req) => {
+      return prisma.user.findUnique({
+        where: {
           email: req.input.email,
         },
       });
