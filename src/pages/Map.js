4526c1cce37f155f8e39f2926a19fcf2020b5ceb
yutengz2 <73250931+pastezzz@users.noreply.github.com>
2023-03-06 03:25:35 -0600

    Update Map.js

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,23 @@
-import { useMemo } from "react";
+import { useState, useMemo } from "react";
+import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-export default function Home() {
+export default function Places() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
   });
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -11,11 +25,62 @@ export default function Home() {
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 40.1164, lng: 88.2434 }), []);
-
+  // 40.1063509,-88.2272382
+  const center = useMemo(() => ({ lat: 40.1063509, lng: -88.2272382 }), []); 
+  const [selected, setSelected] = useState(null);
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+
+      <GoogleMap
+        zoom={16}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        {selected && <Marker position={selected} />}
+        <Marker position={center} label="X" />
+      </GoogleMap>
+    </>
   );
 }
+
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search an address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
