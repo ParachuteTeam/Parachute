63a9c976744da0e23102f053f045fac457b44d17
Haiyue Zhang <andy20010630@outlook.com>
2023-03-08 16:21:11 -0600

    Switch to Next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,30 +2,20 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Auth0Provider } from "@auth0/auth0-react";
-import Layout from "../components/Layout";
 import { api } from "../utils/api";
 import "../styles/globals.css";
+import type { AppProps } from "next/app"
 
-const MyApp: AppType<{ session: Session | null }> = ({
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Auth0Provider
-        domain="dev-nvzp38s2okb5rvuh.us.auth0.com"
-        clientId="HSydcrGc8WCUKW8QUUTWtvCQ3RkOJcCf"
-        authorizationParams={{
-          redirect_uri: "http://localhost:3000",
-        }}
-      >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Auth0Provider>
-      ,
+      <Component {...pageProps} />
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,20 @@
-import NextAuth from "next-auth";
-import { authOptions } from "../../../server/auth";
+import NextAuth from "next-auth"
+import Auth0Provider from "next-auth/providers/auth0";
+import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth(authOptions);
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER
+    }),
+    // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
