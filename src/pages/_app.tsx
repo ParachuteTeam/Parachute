7a71a9b7788fd8153fcbb6870bc201330a2db7f4
Haiyue Zhang <andy20010630@outlook.com>
2023-03-15 23:24:48 -0500

    Delete Auth0 package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { api } from "../utils/api";
 import "../styles/globals.css";
 import type { AppProps } from "next/app"
