3f5bad0f3a08a8b7974ce231d7f838b52e8227dd
RaymondWHZ <wuhaozhen1106@gmail.com>
2023-03-20 19:48:10 -0500

    refactor: inline landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,40 @@
 import { type NextPage } from "next";
-import LandingPage from "../components/LandingPage";
+import Navbar from "../components/Navbar";
+import React from "react";
 
 const Home: NextPage = () => {
-  // use component here because this route is used by both landing page and dashboard,
-  // and we want to custom navbar on the landing page
   return (
     <>
-      <LandingPage />
+      <Navbar />
+      <div className="flex flex-col md:flex-row items-center justify-center w-screen h-screen p-12 gap-16">
+        <div className="flex flex-col items-center md:items-start justify-center w-full md:w-[400px] gap-4">
+          <div className="text-5xl font-bold">
+            🪂
+          </div>
+          <div className="text-5xl font-bold">
+            when2meet
+          </div>
+          <div className="text-6xl font-bold text-orange-elevated mt-[-4px]">
+            Elevated
+          </div>
+          <div className="text-gray-500 mt-2">
+            Open-source, easy-to-use, free forever
+          </div>
+        </div>
+        <div className="w-full sm:max-w-[350px] h-[250px] bg-colorful rounded-[8px] p-[2px] drop-shadow-pink">
+          <div className="flex flex-col items-center justify-center w-full h-full bg-white rounded-[6px] p-8 gap-4">
+            <div className="text-sm text-gray-500 mb-2">
+              Try Parachute right now
+            </div>
+            <button className="bg-black text-white text-center font-semibold rounded-lg w-full p-3">
+              Sign in with Google
+            </button>
+            <button className="border text-center font-semibold rounded-lg w-full p-3">
+              Sign in with OAuth
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
