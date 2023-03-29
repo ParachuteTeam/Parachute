4d48d41a6bc5d2e995e441aeb99d571aace5a509
yutengz2 <73250931+pastezzz@users.noreply.github.com>
2023-03-29 02:36:20 -0500

    Update eventRoute.ts

--- src/server/api/routers/eventRoute.ts
+++ src/server/api/routers/eventRoute.ts
@@ -3,69 +3,75 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 import { PrismaClient } from "@prisma/client";
-import Email from "next-auth/providers/email";
+import { TRPCError, initTRPC } from '@trpc/server';
 
 const prisma = new PrismaClient();
+
+type User = {
+    id: string
+    name: string
+    email: string
+    emailVerified: Date
+    image: string
+    createdAt: Date
+    updatedAt: Date
+  }
+
+type Event = {
+    id: string
+    occuringAt: Date
+    ownerID: string
+    address: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+type Participate = {
+    eventID: string
+    userID: Date
+    timeSlots: JSON
+}
 
 export const eventRouter = createTRPCRouter({
     /*
         async function that creates a new event.
-        First check existance of user according to the email.
-        Then, create new event, participate and update event list.
-        Finally return the updated host of event.
+        First check existence of user according to the email.
+        Then, create new event, participate.
     */
     createEvent: protectedProcedure
         .input(
             z.object({
-                occuringAt: z.string().datetime(), //"2020-01-01T00:00:00Z"
-                owner: z.string(),
-                ownerID: z.string().uuid(),
+                email: z.string().email(),
                 address: z.string(),
-                participant: z.array(z.string()),
-                email: z.string().email()
             })
         )
         .mutation(async (req) => {
-            const userCheck = await prisma.User.findUnique({
+            const userCheck : User | null = await prisma.user.findUnique({
                 where: {
                     email: req.input.email,
                 },
             });
             if (!userCheck) {
-                throw new Error("User does not exist");
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'User does not exist',
+                  });
             }
-            const newEvent = await prisma.event.create({
+            const newEvent : Event = await prisma.event.create({
                 data: {
-                    occuringAt: req.input.occuringAt,
-                    owner: req.input.owner,
-                    ownerID: req.input.ownerID,
+                    occuringAt: new Date(), //"2020-01-01T00:00:00Z"
+                    ownerID: userCheck.id,
                     address: req.input.address,
-                    participant: req.input.participant,
                 },
             });
-            const newParticipate = await prisma.Participate.create({
+            const newParticipate : Participate = await prisma.Participate.create({
                 data: {
-                    event: newEvent,
                     eventID: newEvent.id,
-                    user: userCheck,
                     userID: userCheck.id,
                     timeSlots: {} = {},
                 },
             });
-            return await prisma.user.update({
-                where: {
-                    email: req.input.email,
-                },
-                data: {
-                    event: {
-                        push: newEvent,
-                    },
-                    participate: {
-                        push: newParticipate,
-                    },
-                    updatedAt: new Date(),
-                },
-              });
+            return newEvent;
         }),
     /*
         async function that get whole event list of an user.
@@ -74,128 +80,42 @@ export const eventRouter = createTRPCRouter({
     getEventList: protectedProcedure
         .input(z.object({ email: z.string().email() }))
         .query(async (req) => {
-            const userCheck = await prisma.User.findUnique({
+            const userCheck : User | null = await prisma.User.findUnique({
                 where: {
                     email: req.input.email,
                 },
             });
             if (!userCheck) {
-                throw new Error("User does not exist");
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'User does not exist',
+                });
             }
-            return await prisma.user.findUnique({
-                where: { email: req.input.email },
-                // select: { id: true, event: true },
+            return await prisma.Event.findMany({
+                where: { ownerID: userCheck.id },
+                select: { id: true },
             });
         }),
         
-    // getEvent: protectedProcedure
-    //     .input(z.object({ 
-    //         email: z.string().email(),
-    //         eventId: z.string()
-    //     }))
-    //     .query(async (req) => {
-    //         const userCheck = await prisma.User.findUnique({
-    //             where: {
-    //                 email: req.input.email,
-    //             },
-    //         });
-    //         if (!userCheck) {
-    //             throw new Error("User does not exist");
-    //         }
-    //         const eventCheck = await prisma.Event.findUnique({
-    //             where: {
-    //                 id: req.input.eventId,
-    //             },
-    //         });
-    //         if (!eventCheck) {
-    //             throw new Error("Event does not exist");
-    //         }
-    //         const participantCheck = await prisma.Participate.findUnique({
-    //             where: {
-    //                 userID: userCheck.id,
-    //                 eventID: eventCheck.id,
-    //             },
-    //         });
-    //         if (!participantCheck) {
-    //             throw new Error("User is not qualified");
-    //         }
-    //         return eventCheck;
-    //     }),
-
-    /*
-        Update the participate list of a event. Add a user in the end of participate list.
-        Check whether the event and user exists and only host of event can add participant.
-        Also update lists in user, event and create new participate object.
-        Finally return the event.
-    */
-    updateEvent_list: protectedProcedure
+    getEvent: protectedProcedure
         .input(z.object({ 
-            host_email: z.string().email(),
-            person_to_add: z.string().email(),
             eventId: z.string()
         }))
-        .mutation(async (req) => {
-            const userCheck = await prisma.User.findUnique({
-                where: {
-                    email: req.input.host_email,
-                },
-            });
-            if (!userCheck) {
-                throw new Error("User does not exist");
-            };
-            const eventCheck = await prisma.Event.findUnique({
+        .query(async (req) => {
+            const eventCheck : Event | null = await prisma.Event.findUnique({
                 where: {
                     id: req.input.eventId,
                 },
             });
             if (!eventCheck) {
-                throw new Error("Event does not exist");
-            };
-            const personCheck = await prisma.Event.findUnique({
-                where: {
-                    id: req.input.person_to_add,
-                },
-            });
-            if (!personCheck) {
-                throw new Error("Event does not exist");
-            };
-            if (userCheck.id != eventCheck.ownerID) {
-                throw new Error("not qualified");
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'Event does not exist',
+                });
             }
-            const newParticipate = await prisma.Participate.create({
-                data: {
-                    event: eventCheck,
-                    eventID: eventCheck.id,
-                    user: userCheck,
-                    userID: userCheck.id,
-                    timeSlots: {} = {},
-                },
-            });
-            const event_update = await prisma.event.update({
-                where: {
-                    id: req.input.eventId,
-                },
-                data: {
-                    participate: {
-                        push: newParticipate,
-                    },
-                },
-            });
-            await prisma.user.update({
-                where: {
-                    id: userCheck.id,
-                },
-                data: {
-                    participate: {
-                        push: newParticipate,
-                    },
-                    event: {
-                        push: event_update
-                    }
-                },
-            });
-            return event_update;
+            return eventCheck;
         }),
+
     /**
      * Verify the status of event host and check the existance of event. 
      * Then update event address.
@@ -207,21 +127,33 @@ export const eventRouter = createTRPCRouter({
             eventId: z.string()
         }))
         .mutation(async (req) => {
-            const eventCheck = await prisma.Event.findUnique({
+            const eventCheck : Event | null = await prisma.Event.findUnique({
                 where: {
                     id: req.input.eventId,
                 },
             });
             if (!eventCheck) {
-                throw new Error("Event does not exist");
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'Event does not exist',
+                });
             }
-            const userCheck = await prisma.User.findUnique({
+            const userCheck : User | null = await prisma.User.findUnique({
                 where: {
                     email: req.input.host_email,
                 },
             });
+            if (!userCheck) {
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'User does not exist',
+                });
+            }
             if (userCheck.id != eventCheck.ownerID ) {
-                throw new Error("not qualified");
+                throw new TRPCError({
+                    code: 'UNAUTHORIZED',
+                    message: 'User is not event holder',
+                });
             };
             return await prisma.event.update({
                 where: {
@@ -243,21 +175,33 @@ export const eventRouter = createTRPCRouter({
             eventId: z.string()
         }))
         .mutation(async (req) => {
-            const eventCheck = await prisma.Event.findUnique({
+            const eventCheck : Event | null = await prisma.Event.findUnique({
                 where: {
                     id: req.input.eventId,
                 },
             });
             if (!eventCheck) {
-                throw new Error("Event does not exist");
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'Event does not exist',
+                });
             }
-            const userCheck = await prisma.User.findUnique({
+            const userCheck : User | null = await prisma.User.findUnique({
                 where: {
                     email: req.input.host_email,
                 },
             });
+            if (!userCheck) {
+                throw new TRPCError({
+                    code: 'FORBIDDEN',
+                    message: 'User does not exist',
+                });
+            }
             if (userCheck.id != eventCheck.ownerID ) {
-                throw new Error("not qualified");
+                throw new TRPCError({
+                    code: 'UNAUTHORIZED',
+                    message: 'User is not event holder',
+                });
             };
             return await prisma.user.delete({
                 where: {
