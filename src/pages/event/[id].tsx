2535e160b0c13d4a4792d6f419d6f3e0fcef736c
RaymondWHZ <wuhaozhen1106@gmail.com>
2023-05-02 12:00:00 -0500

    style: refactor and update dialog styles

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -17,221 +17,257 @@ import {
   GoogleLoginButton,
 } from "../../components/LoginButton";
 import { currentTimezone } from "../../utils/timezone";
-import { Dialog, Transition } from '@headlessui/react'
-import { useState } from 'react'
+import { Dialog, Transition } from "@headlessui/react";
+import { useState } from "react";
 import { api } from "../../utils/api";
+
+interface EditDialogProps {
+  isOpen: boolean;
+  close: () => void;
+  eventName: string;
+  onSubmit: (eventName: string) => void;
+}
+
+const EditDialog: React.FC<EditDialogProps> = ({
+  isOpen,
+  close,
+  eventName,
+  onSubmit,
+}) => {
+  const [newEventName, setNewEventName] = useState(eventName);
+  return (
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={() => close()}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          ></span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="my-space-x-8 inline-block w-[400px] transform rounded-xl bg-white p-7 shadow-xl transition-all">
+              <Dialog.Title className="text-left text-xl font-semibold leading-6 text-gray-900">
+                Edit Event Name
+              </Dialog.Title>
+              <div className="input-field mt-5 text-left">
+                <label>Event Name</label>
+                <input
+                  type="text"
+                  className="rounded-input text-sm"
+                  placeholder="Enter event name here..."
+                  value={newEventName}
+                  onChange={(e) => setNewEventName(e.target.value)}
+                />
+                <div className="text-left text-xs text-gray-500">
+                  You can only edit event name after creating the event. <br />
+                  To change occurring days and time span, delete the event and
+                  create a new one.
+                </div>
+              </div>
+              <div className="mt-5 flex justify-center gap-4">
+                <button
+                  className="rounded-button w-[50%] text-sm"
+                  onClick={() => close()}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="primary-button-with-hover w-[50%] text-sm font-normal"
+                  onClick={() => {
+                    if (!newEventName) {
+                      alert("Empty EventName is Not Allowed.");
+                      return;
+                    }
+                    onSubmit(newEventName);
+                    close();
+                  }}
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+interface DeleteDialogProps {
+  isOpen: boolean;
+  close: () => void;
+  eventName: string;
+  onSubmit: () => void;
+}
+
+const DeleteDialog: React.FC<DeleteDialogProps> = ({
+  isOpen,
+  close,
+  eventName,
+  onSubmit,
+}) => {
+  return (
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={() => close()}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          ></span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="my-space-x-8 inline-block w-[450px] max-w-lg transform rounded-xl bg-white p-6 shadow-xl transition-all">
+              <Dialog.Title className="text-left text-lg font-semibold leading-6 text-gray-900">
+                Confirm Delete Event
+              </Dialog.Title>
+              <div className="mt-4 text-left text-2xl font-bold">
+                {eventName}
+              </div>
+              <div className="mt-2 text-left text-xs text-gray-500">
+                The event will be permanently deleted, including all
+                availability data. This action is irreversible and can not be
+                undone.
+              </div>
+              <div className="mt-6 flex justify-center gap-4">
+                <button
+                  className="rounded-button w-[50%] text-sm"
+                  onClick={() => close()}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="danger-button w-[50%] text-sm"
+                  onClick={() => {
+                    onSubmit();
+                    close();
+                  }}
+                >
+                  Permanently Delete
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
 const EventInfoHeader: React.FC = () => {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
   const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
 
-  const handleDeleteClick = () => {
-    setIsDeleteDialogOpen(true);
-  };
-  const handleEditClick = () => {
-    setIsEditDialogOpen(true);
-  };
-
   const deleteEvent = api.events.deleteEvent.useMutation();
   const editEventName = api.events.updateEvent_name.useMutation();
 
-  const [eventName, setEventName] = useState("");
-
   return (
-      <div className="flex w-full flex-row justify-center border-t border-gray-200 bg-white px-12 py-6">
-        <div className="flex max-w-[1200px] flex-1 flex-row items-center gap-2">
-          <div className="flex flex-1 flex-col gap-2">
-            <div className="flex flex-row items-center gap-1 text-sm text-gray-500">
-              <MdOutlineCalendarToday />
-              <div>Sun, Wed, Thu</div>
-              <MdOutlineAccessTime className="ml-1" />
-              <div>12:00 PM - 1:00 PM</div>
-            </div>
-            <div className="mb-0.5 text-3xl font-semibold">
-              CS 222 Group Meeting
-            </div>
-            <div className="flex flex-row items-center gap-2 text-sm">
-              <EventTypeTag>My Event</EventTypeTag>
-              <p>No one filled yet</p>
-              <p>
-                <span className="font-bold">Event ID:</span> 123456
-              </p>
-              <p>
-                <span className="font-bold">Link:</span>{" "}
-                https://parachute.fyi/event/123456
-              </p>
-            </div>
+    <div className="flex w-full flex-row justify-center border-t border-gray-200 bg-white px-12 py-6">
+      <div className="flex max-w-[1200px] flex-1 flex-row items-center gap-2">
+        <div className="flex flex-1 flex-col gap-2">
+          <div className="flex flex-row items-center gap-1 text-sm text-gray-500">
+            <MdOutlineCalendarToday />
+            <div>Sun, Wed, Thu</div>
+            <MdOutlineAccessTime className="ml-1" />
+            <div>12:00 PM - 1:00 PM</div>
           </div>
-          <div className="flex w-[200px] flex-col gap-3 text-sm font-light">
-            <button className="rounded-button" onClick={handleEditClick}>Edit</button>
-            <button className="danger-button" onClick={handleDeleteClick}>Delete</button>
+          <div className="mb-0.5 text-3xl font-semibold">
+            CS 222 Group Meeting
+          </div>
+          <div className="flex flex-row items-center gap-2 text-sm">
+            <EventTypeTag>My Event</EventTypeTag>
+            <p>No one filled yet</p>
+            <p>
+              <span className="font-bold">Event ID:</span> 123456
+            </p>
+            <p>
+              <span className="font-bold">Link:</span>{" "}
+              https://parachute.fyi/event/123456
+            </p>
           </div>
         </div>
-        <Transition show={isDeleteDialogOpen} as={Fragment}>
-          <Dialog
-              as="div"
-              className="fixed inset-0 z-10 overflow-y-auto"
-              open={isDeleteDialogOpen}
-              onClose={() => setIsDeleteDialogOpen(false)}
+        <div className="flex w-[200px] flex-col gap-3 text-sm font-light">
+          <button
+            className="rounded-button"
+            onClick={() => setIsEditDialogOpen(true)}
           >
-            <div className="min-h-screen px-4 text-center">
-              <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
-              </Transition.Child>
-              <span
-                  className="inline-block h-screen align-middle"
-                  aria-hidden="true"
-              >
-            </span>
-              <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95"
-              >
-                <div className="inline-block w-full max-w-lg p-6 my-space-x-8 transition-all transform bg-white shadow-xl rounded-2xl">
-                  <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900 text-left"
-                  >
-                    Confirm deletion
-                  </Dialog.Title>
-                  <Dialog.Description className="mt-2 text-2xl font-bold text-left">
-                    CS 222 Group Meeting
-                  </Dialog.Description>
-                  <Dialog.Description className="mt-2 text-sm text-gray-500">
-                    The event will be permanently deleted, including all availability data. This action is irreversible and can not be undone.
-                  </Dialog.Description>
-                  <div className="mt-4 flex mx-auto justify-center gap-4">
-                    <button
-                        className="w-60 inline-flex justify-center rounded-md border border-grey-200 bg-transparent px-4 py-2 text-sm font-medium text-black-900 hover:bg-black-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                        onClick={() => setIsDeleteDialogOpen(false)}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                        className="w-60 px-4 py-2 text-sm danger-button"
-                        onClick={() => {
-                          deleteEvent.mutate({
-                            host_email: 'haiyuezhang63@gmail.com',
-                            eventId: '568b0bed-63c6-4296-8e98-0c74e30f3391'
-                          });
-                          setIsDeleteDialogOpen(false);
-                        }}
-                    >
-                      Permanently Delete
-                    </button>
-                  </div>
-                </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition>
-        <Transition show={isEditDialogOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="fixed inset-0 z-10 overflow-y-auto"
-            open={isEditDialogOpen}
-            onClose={() => setIsEditDialogOpen(false)}
+            Edit
+          </button>
+          <button
+            className="danger-button"
+            onClick={() => setIsDeleteDialogOpen(true)}
           >
-            <div className="min-h-screen px-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
-              </Transition.Child>
-              <span
-                className="inline-block h-screen align-middle"
-                aria-hidden="true"
-              >
-            </span>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <div className="inline-block w-full max-w-md p-6 my-space-x-8 transition-all transform bg-white shadow-xl rounded-2xl">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-bold leading-6 text-gray-900 text-left"
-                  >
-                    Edit Event Name
-                  </Dialog.Title>
-                  <div className="mt-4" />
-                  <Dialog.Title
-                    as="h3"
-                    className="text-sm font-bold leading-6 text-gray-900 text-left"
-                  >
-                    Event Name
-                  </Dialog.Title>
-                  <Dialog.Description className="mt-2 text-sm text-gray-500">
-                    <input
-                      type="text"
-                      className="border border-gray-250 p-3 rounded-md w-full"
-                      style={{ paddingRight: "10px" }}
-                      placeholder="Enter Event Name Here..."
-                      value={eventName}
-                      onChange={(e) => setEventName(e.target.value)}
-                    />
-                  </Dialog.Description>
-                  <div className="mt-3" />
-                  <Dialog.Title
-                    as="h3"
-                    className="text-xs leading-4 text-gray-600 text-left"
-                  >
-                    You can only edit event name after creating the event. To change occurring days and time span, delete the event and create a new one.
-                  </Dialog.Title>
-                  <div className="mt-4 flex mx-auto justify-center gap-4">
-                    <button
-                      className="w-60 inline-flex justify-center rounded-md border border-grey-200 bg-transparent px-4 py-2 text-sm font-medium text-black-900 hover:bg-black-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={() => setIsEditDialogOpen(false)}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="w-60 px-4 py-2 text-sm danger-button bg-black"
-                      onClick={() => {
-                        if (!eventName) {
-                          alert("Empty EventName is Not Allowed.");
-                          return;
-                        }
-                        editEventName.mutate({
-                          host_email: 'haiyuezhang63@gmail.com',
-                          name: eventName,
-                          eventId: '568b0bed-63c6-4296-8e98-0c74e30f3391'
-                        });
-                        setIsEditDialogOpen(false);
-                      }}
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition>
+            Delete
+          </button>
+        </div>
       </div>
+      <EditDialog
+        isOpen={isEditDialogOpen}
+        close={() => setIsEditDialogOpen(false)}
+        eventName={"CS 222 Group Meeting"}
+        onSubmit={(newEventName) => {
+          editEventName.mutate({
+            host_email: "haiyuezhang63@gmail.com",
+            name: newEventName,
+            eventId: "568b0bed-63c6-4296-8e98-0c74e30f3391",
+          });
+        }}
+      />
+      <DeleteDialog
+        isOpen={isDeleteDialogOpen}
+        close={() => setIsDeleteDialogOpen(false)}
+        eventName={"CS 222 Group Meeting"}
+        onSubmit={() => {
+          deleteEvent.mutate({
+            host_email: "haiyuezhang63@gmail.com",
+            eventId: "568b0bed-63c6-4296-8e98-0c74e30f3391",
+          });
+        }}
+      />
+    </div>
   );
 };
 
