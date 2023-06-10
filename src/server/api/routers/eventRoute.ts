aea426507b467fa302c0665fe7113e516f038f80
yutengz2 <73250931+pastezzz@users.noreply.github.com>
2023-06-10 17:51:30 +0800

    Update eventRoute.ts to adapt new schema

--- src/server/api/routers/eventRoute.ts
+++ src/server/api/routers/eventRoute.ts
@@ -120,6 +120,7 @@ export const eventRouter = createTRPCRouter({
           joinCode: true,
           occuringDays: true,
           type: true,
+          partCount: true,
         },
         orderBy: { begins: "asc" },
       });
