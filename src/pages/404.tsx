26c4a8b660ba064ecd07f12fadaa09e3d9bed3c1
Haiyue Zhang <andy20010630@outlook.com>
2024-10-01 15:36:55 -0400

    Solve error message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Navbar from "../components/section/Navbar";
 import Footer from "../components/section/Footer";
